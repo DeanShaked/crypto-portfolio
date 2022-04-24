3a35ee5118c24986df6140d90fb13b541acb4673
Dean Shaked <deanshak9@gmail.com>
2022-04-24 23:53:54 +0300

    added ethereum

--- src/components/TableContainer/DataMock.js
+++ src/components/TableContainer/DataMock.js
@@ -19,4 +19,24 @@ export const DataMock = {
       platform: "Crypto.com",
     },
   ],
+  ETH: [
+    {
+      symbol: "ETHUSD",
+      symbolName: "Ethereum",
+      amount: 0.01,
+      openRate: 58769,
+      currentRate: 0,
+      totalPrice: amount * openRate,
+      platform: "Crypto.com",
+    },
+    {
+      symbol: "BTCUSD",
+      symbolName: "Bitcoin",
+      amount: 0.5,
+      openRate: 40276,
+      currentRate: 0,
+      totalPrice: amount * openRate,
+      platform: "Crypto.com",
+    },
+  ],
 };
