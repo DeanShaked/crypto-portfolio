8cb3bbaf5e942ade7b437f3648e55789523490ee
dean shaked <deanshak9@gmail.com>
2022-05-12 15:59:48 +0300

    ...

--- server/controller/tradeController/tradeController.js
+++ server/controller/tradeController/tradeController.js
@@ -1,14 +1,34 @@
-const AssetSchema = require('../../models/AssetSchema');
+const AssetSchema = require("../../models/AssetSchema");
 
 exports.createNewAsset = async (req, res, next) => {
   try {
     const body = req.body;
-    console.log('body', body);
+    console.log("body", body);
     // Send new asset to DB
     const newAsset = await AssetSchema(body);
     await newAsset.save();
-    return res.status(200).json({ message: 'New Trade!' });
+    return res.status(200).json({ message: body });
   } catch (error) {
-    return res.status(401).json({ message: 'Error!' });
+    return res.status(401).json({ message: error });
+  }
+};
+
+exports.getAllPositions = async (req, res, next) => {
+  try {
+    const allPositions = await AssetSchema.find();
+    return res.status(200).json({ message: allPositions });
+  } catch (error) {
+    return res.status(401).json({ message: error });
+  }
+};
+
+exports.removePosition = async (req, res, next) => {
+  try {
+    const { positionId } = req;
+    const positionDelted = AssetSchema.find(positionId);
+    const res = AssetSchema.findByIdAndDelete(positionId);
+    return res.status(200).json({ message: positionDelted });
+  } catch (error) {
+    return res.status(401).json({ message: error });
   }
 };
--- server/routes/tradeRoutes/tradeRoutes.js
+++ server/routes/tradeRoutes/tradeRoutes.js
@@ -1,8 +1,10 @@
-const express = require('express');
-const tradeController = require('../../controller/tradeController/tradeController');
+const express = require("express");
+const tradeController = require("../../controller/tradeController/tradeController");
 
 const route = express.Router();
 
-route.post('/new-asset', tradeController.createNewAsset);
+route.post("/new-asset", tradeController.createNewAsset);
+route.get("/get-all-positions", tradeController.getAllPositions);
+route.get("/delete", tradeController.removePosition);
 
 module.exports = route;
